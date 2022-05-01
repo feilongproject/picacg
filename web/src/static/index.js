93496625745eee7550bb1dc48ee0f1b0f1b63522
feilongproject <flzs6137@gmail.com>
2022-05-01 13:15:51 +0800

    fix(index.js): 修复新建项目时部分文件出错
    从bili-downloader项目创建时，index.js文件中，欢迎内容未能及时更改，现已修复，应该没有类似错误了

--- web/src/static/index.js
+++ web/src/static/index.js
@@ -1,7 +1,6 @@
-
-console.log("欢迎使用bilibili视频下载助手");
+console.log("欢迎使用PICACG web.ver");
 console.log("%c 作者 -> 飞龙project ", "background:#000; color:#fff");
 console.log(
-    "%c GitHub -> https://github.com/feilongproject/bili-downloader ",
+    "%c GitHub -> https://github.com/feilongproject/picacg ",
     "background:#000; color:#fff"
 );
