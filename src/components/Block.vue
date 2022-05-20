9d9c8e9f48fda79ab1f5ab97f3e697d03418480c
feilongproject <flzs6137@gmail.com>
2022-05-21 01:06:28 +0800

    feat: 添加分类获取功能

---
+++ src/components/Block.vue
@@ -0,0 +1,63 @@
+<template>
+  <el-main class="main">
+    <div class="images">
+      <div v-for="(item, index) in blocks" :key="index">
+        <el-image
+          v-if="!item.active"
+          :src="`${ApiProxyUrl}storage?fileServer=${item.thumb.fileServer}&path=${item.thumb.path}`"
+          fit="contain"
+        >
+        </el-image>
+        <el-tag v-if="!item.active" effect="dark"> {{ item.title }}</el-tag>
+      </div>
+    </div>
+  </el-main>
+</template>
+
+<style lang='scss'>
+.images {
+  display: flex;
+  flex-wrap: wrap;
+  div {
+    display: flex;
+    flex-direction: column;
+    .el-image {
+      width: 200px;
+      height: 200px;
+    }
+    .el-tag {
+      align-self: center;
+    }
+  }
+}
+</style>
+
+<script lang='ts'>
+import Vue from "vue";
+import { CustomConfig } from "../config";
+export default Vue.extend({
+  data: () => {
+    return {
+      blocks: Array(),
+      ApiProxyUrl: CustomConfig.ApiProxyUrl,
+    };
+  },
+  mounted: async function () {
+    var blocks: ComicsCategories = await fetch(
+      `${CustomConfig.ApiProxyUrl}comics/categories`
+    )
+      .then((res) => {
+        return res.text();
+      })
+      .then((text) => {
+        //console.log(text);
+        return JSON.parse(text);
+      });
+    console.log(blocks);
+    this.blocks = blocks.data.categories;
+
+    console.log("finish");
+  },
+});
+</script>
+
---
+++ src/pages/block.vue
@@ -0,0 +1,8 @@
+<template>
+  <keep-alive>
+    <el-container direction="vertical">
+      <Title />
+      <Block />
+    </el-container>
+  </keep-alive>
+</template>
