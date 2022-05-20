2cf91778812addc82254597f67226c969db28edb
feilongproject <flzs6137@gmail.com>
2022-05-20 23:06:42 +0800

    refactor: 分离项目，使web内容独立

--- api/src/mod/getSign.ts
+++
@@ -1,9 +0,0 @@
-import hmacSHA256 from '../CryptoJS/hmac-sha256';
-
-
-export function getSign(url: string, timestamp: string, nonce: string, method: "GET" | "POST",): string {
-
-    var key = url + timestamp + nonce + method + "C69BAF41DA5ABD1FFEDC6D2FEA56B";
-
-    return hmacSHA256(key.toLowerCase(), "~d}$Q7$eIni=V)9\\RK/P.RM4;9[7|@/CA}b~OW!3?EV`:<>M7pddUBL5n|0/*Cn").toString();
-}
